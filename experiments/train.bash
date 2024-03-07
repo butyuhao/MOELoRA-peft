43781588ed655454bf4a1c29e0b7fe1d1aec2eef
butyuhao <butyuhao@foxmail.com>
2024-03-07 04:15:18 +0000

    update

---
+++ experiments/train.bash
@@ -0,0 +1,43 @@
+lora_rank=16
+lora_trainable="gate_proj,k_proj,o_proj,down_proj,v_proj,q_proj,up_proj"
+modules_to_save="null"
+lora_dropout=0.1
+LR=2e-4
+MAX_STEPS=8000
+SAVE_STEPS=8000
+MASTER_PORT=$(shuf -n 1 -i 10000-65535)
+model_name_or_path="/cpfs01/user/chenqin.p/model_cached/Llama-2-7b-chat-hf"   
+your_data_path="data"  
+your_checkpopint_path="saved/moelora"  
+MAX_SOURCE_LENGTH=1024
+
+peft_path=""  
+
+# # Training Command
+deepspeed --num_gpus=4 --master_port $MASTER_PORT run_mlora.py \
+    --deepspeed src/ds.config \
+    --do_train \
+    --train_file $your_data_path/train.json \
+    --cache_dir $your_data_path \
+    --prompt_column input \
+    --response_column target \
+    --overwrite_cache \
+    --model_name_or_path $model_name_or_path \
+    --output_dir $your_checkpopint_path \
+    --overwrite_output_dir \
+    --max_source_length $MAX_SOURCE_LENGTH \
+    --max_target_length 196 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --max_steps ${MAX_STEPS} \
+    --logging_steps 100 \
+    --save_steps ${SAVE_STEPS} \
+    --learning_rate $LR \
+    --lora_rank ${lora_rank} \
+    --trainable ${lora_trainable} \
+    --modules_to_save ${modules_to_save} \
+    --lora_dropout ${lora_dropout} \
+    --fp16 \
+    --lora_name moelora \
+    --expert_num 10
